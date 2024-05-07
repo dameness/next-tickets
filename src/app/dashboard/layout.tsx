84cc45323cec0fde85f4d737e5aa6b5d46b3f4a2
bernardo damiani <bmdamiani17@gmail.com>
2024-05-07 15:33:24 -0300

    dashboard layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <div className="w-[96%] flex items-center gap-4 p-4 rounded-lg bg-neutral-800 text-neutral-200">
+        <Link href="/dashboard/tickets">Tickets</Link>
+        <Link href="/dashboard/customers">Customers</Link>
+      </div>
+      {children}
+    </>
+  );
+}
