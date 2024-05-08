385468cee1e9808c7aa4a770374eba75cdf49809
bernardo damiani <bmdamiani17@gmail.com>
2024-05-08 13:18:35 -0300

    new customer component

---
+++ src/app/dashboard/customers/new/page.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+export default function NewCustomer() {
+  return (
+    <div className="mt-4 flex flex-col w-full gap-2">
+      <div className="flex items-center gap-2 mb-2">
+        <Link
+          href="/dashboard/customers"
+          className="px-4 py-2 text-white bg-neutral-800 rounded-lg"
+        >
+          Return
+        </Link>
+        <h1 className="text-3xl font-semibold">New customer</h1>
+      </div>
+      <h1 className="text-xl font-semibold">Name</h1>
+      <input
+        className="p-2 rounded-lg w-full border-2"
+        placeholder="Enter the name..."
+      />
+      <div className="grid xs:grid-cols-2 grid-cols-1 gap-2">
+        <div>
+          <h1 className="text-xl font-semibold">Telephone</h1>
+          <input
+            className="p-2 rounded-lg w-full border-2"
+            placeholder="Enter the telephone..."
+          />
+        </div>
+        <div>
+          <h1 className="text-xl font-semibold">E-mail</h1>
+          <input
+            className="p-2 rounded-lg w-full border-2"
+            placeholder="Enter the e-mail..."
+          />
+        </div>
+      </div>
+      <button className="bg-blue-400 rounded-lg w-full p-2 mt-4 text-white font-semibold">
+        Register
+      </button>
+    </div>
+  );
+}
