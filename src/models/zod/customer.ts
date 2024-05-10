4a5da5765e3c30cdd09a76a93b6ef983eb17fadd
Bernardo Damiani <bmdamiani17@gmail.com>
2024-05-10 17:09:34 -0300

    phone validation

--- src/models/zod/customer.ts
+++ src/models/zod/customer.ts
@@ -2,7 +2,16 @@ import { z } from "zod";
 
 export const CustomerSchema = z.object({
   name: z.string().min(1, "Name required"),
-  phone: z.string().min(1, "Phone required"),
+  phone: z
+    .string()
+    .min(1, "Phone required")
+    .refine(
+      (input) =>
+        /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/.test(
+          input
+        ),
+      "Enter a valid phone number"
+    ),
   email: z.string().min(1, "E-mail required").email("Enter a valid e-mail"),
 });
 
