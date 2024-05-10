6b1b9740701a625421611b22071d9aab4457f77a
Bernardo Damiani <bmdamiani17@gmail.com>
2024-05-10 15:24:12 -0300

    export zod schemas

--- src/models/zod/ticket.ts
+++ src/models/zod/ticket.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
-const schema = z.object({
+export const TicketSchema = z.object({
   name: z.string().min(1, { message: "Title required" }),
   description: z.string().min(1, { message: "Description required" }),
   customerId: z.string().min(1, { message: "Select a customer" }),
 });
 
-export type Ticket = z.infer<typeof schema>;
+export type Ticket = z.infer<typeof TicketSchema>;
