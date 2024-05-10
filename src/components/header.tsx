3241ca72a626a9eb09a85fe5b98e66355b96b1dd
Bernardo Damiani <bmdamiani17@gmail.com>
2024-05-10 15:52:11 -0300

    header content max width

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,44 +21,46 @@ export default function Header() {
   }
 
   return (
-    <header className=" bg-neutral-700 text-neutral-200 w-full mb-4 flex justify-between items-center py-2 px-4 h-16">
-      <Link className="font-extrabold text-sm cursor-pointer" href="/">
-        <span className="text-blue-600 text-xl">NEXT</span> TICKETS
-      </Link>
+    <header className=" bg-neutral-700 text-neutral-200 w-full flex items-center justify-center mb-4 py-2 px-4 h-16">
+      <div className="flex justify-between items-center w-full max-w-7xl">
+        <Link className="font-extrabold text-sm cursor-pointer" href="/">
+          <span className="text-blue-600 text-xl">NEXT</span> TICKETS
+        </Link>
 
-      <nav className="flex items-center gap-2">
-        {status === "loading" ? (
-          <LoaderCircle className="animate-spin transition" />
-        ) : status === "authenticated" ? (
-          <>
-            {data.user.image && (
-              <Link href="#">
-                <Image
-                  src={data.user.image || GuestImage}
-                  alt="user"
-                  className="rounded-full border-2 border-red"
-                  width={36}
-                  height={36}
-                />
+        <nav className="flex items-center gap-2">
+          {status === "loading" ? (
+            <LoaderCircle className="animate-spin transition" />
+          ) : status === "authenticated" ? (
+            <>
+              {data.user.image && (
+                <Link href="#">
+                  <Image
+                    src={data.user.image || GuestImage}
+                    alt="user"
+                    className="rounded-full border-2 border-red"
+                    width={36}
+                    height={36}
+                  />
+                </Link>
+              )}
+              <Link href="/tickets">
+                <Settings2 />
               </Link>
-            )}
-            <Link href="/tickets">
-              <Settings2 />
-            </Link>
-            <button onClick={handleLogout}>
-              <LogOut />
+              <button onClick={handleLogout}>
+                <LogOut />
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={handleLogin}
+              className="flex gap-1.5 items-center bg-blue-600 px-4 py-2 rounded-lg text-neutral-200"
+            >
+              <FaGoogle />
+              Sign in
             </button>
-          </>
-        ) : (
-          <button
-            onClick={handleLogin}
-            className="flex gap-1.5 items-center bg-blue-600 px-4 py-2 rounded-lg text-neutral-200"
-          >
-            <FaGoogle />
-            Sign in
-          </button>
-        )}
-      </nav>
+          )}
+        </nav>
+      </div>
     </header>
   );
 }
