47baf9d8acb6888ea62c95aab34e5d11cdf6a268
Bernardo Damiani <bmdamiani17@gmail.com>
2024-05-10 16:41:52 -0300

    zod schema

--- src/models/zod/customer.ts
+++ src/models/zod/customer.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const CustomerSchema = z.object({
   name: z.string().min(1, "Name required"),
   phone: z.string().min(1, "Phone required"),
-  email: z.string().email("Enter a valid e-mail").min(1, "E-mail required"),
+  email: z.string().min(1, "E-mail required").email("Enter a valid e-mail"),
 });
 
 export type Customer = z.infer<typeof CustomerSchema>;
