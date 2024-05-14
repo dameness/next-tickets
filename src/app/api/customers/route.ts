aa0d22e97e23c7da5052c413aa6ee6884e0b1bf4
bernardo damiani <bmdamiani17@gmail.com>
2024-05-14 11:55:38 -0300

    feature: add delete customer route

--- src/app/api/customers/route.ts
+++ src/app/api/customers/route.ts
@@ -26,3 +26,34 @@ export async function POST(req: Request) {
     );
   }
 }
+
+export async function DELETE(req: Request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json({ message: "Not Authorized." }, { status: 401 });
+  }
+
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const id = searchParams.get("id");
+
+    if (!id) {
+      return NextResponse.json(
+        { message: "Invalid Customer ID" },
+        { status: 406 }
+      );
+    }
+
+    await prisma.customer.delete({ where: { id: id } });
+
+    return NextResponse.json({ message: "Success." });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Couldn't delete customer data." },
+      { status: 500 }
+    );
+  }
+}
