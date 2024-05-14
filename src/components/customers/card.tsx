4fd56254894df437318ca1ab8245068e9c9ee8c5
bernardo damiani <bmdamiani17@gmail.com>
2024-05-14 11:56:23 -0300

    feature: deleting customer

--- src/components/customers/card.tsx
+++ src/components/customers/card.tsx
@@ -1,6 +1,28 @@
-import { Customer } from "@/models/zod/customer";
+"use client";
 
-export default function CustomerCard({ name, phone, email }: Customer) {
+import api from "@/config/api";
+import { Customer } from "@/models/zod/customer";
+import { useRouter } from "next/navigation";
+
+export default function CustomerCard({ id, name, phone, email }: Customer) {
+  const router = useRouter();
+  async function handleDeleteCustomer() {
+    api
+      .delete(`/customers`, {
+        params: { id },
+      })
+      .then(() => {
+        alert("Customer deleted!");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Error deleting customer!");
+      })
+      .finally(() => {
+        router.refresh();
+      });
+  }
+
   return (
     <div className="flex flex-col justify-between gap-1 p-2 border-2 max-w-sm rounded-lg">
       <div className="flex-flex-col overflow-hidden">
@@ -14,7 +36,10 @@ export default function CustomerCard({ name, phone, email }: Customer) {
           <span className="font-semibold">E-mail: </span> {email}
         </h1>
       </div>
-      <button className="mt-2 px-2 bg-red-400 text-white rounded-lg mx-auto w-1/2 max-w-32">
+      <button
+        onClick={handleDeleteCustomer}
+        className="mt-2 px-2 bg-red-400 text-white rounded-lg mx-auto w-1/2 max-w-32"
+      >
         Delete
       </button>
     </div>
