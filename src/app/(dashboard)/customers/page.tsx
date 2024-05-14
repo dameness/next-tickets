efb5b7a62e4ae6bef3de5f45081b6514514f6145
bernardo damiani <bmdamiani17@gmail.com>
2024-05-14 10:46:36 -0300

    refactor: change card styles

--- src/app/(dashboard)/customers/page.tsx
+++ src/app/(dashboard)/customers/page.tsx
@@ -23,7 +23,7 @@ export default async function Customers() {
           New customer
         </Link>
       </div>
-      <div className="grid sm:grid-cols-3 grid-cols-2 gap-x-2 gap-y-4">
+      <div className="grid md:grid-cols-3 grid-cols-2 gap-x-2 gap-y-4">
         {customers.map((customer, index) => (
           <Card
             key={index}
--- src/components/customers/card.tsx
+++ src/components/customers/card.tsx
@@ -2,16 +2,18 @@ import { Customer } from "@/models/zod/customer";
 
 export default function CustomerCard({ name, phone, email }: Customer) {
   return (
-    <div className="flex flex-col gap-1 p-2 border-2 max-w-sm rounded-lg">
-      <h1>
-        <span className="font-semibold">Name: </span> {name}
-      </h1>
-      <h1>
-        <span className="font-semibold">Telephone: </span> {phone}
-      </h1>
-      <h1>
-        <span className="font-semibold">E-mail: </span> {email}
-      </h1>
+    <div className="flex flex-col justify-between gap-1 p-2 border-2 max-w-sm rounded-lg">
+      <div className="flex-flex-col">
+        <h1>
+          <span className="font-semibold">Name: </span> {name}
+        </h1>
+        <h1>
+          <span className="font-semibold">Telephone: </span> {phone}
+        </h1>
+        <h1>
+          <span className="font-semibold">E-mail: </span> {email}
+        </h1>
+      </div>
       <button className="mt-2 px-2 bg-red-400 text-white rounded-lg mx-auto w-1/2 max-w-32">
         Delete
       </button>
