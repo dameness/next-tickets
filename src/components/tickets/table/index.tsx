5cfb5ebe8224993df5eb14354c77c7f66d68c95b
bernardo damiani <bmdamiani17@gmail.com>
2024-05-15 10:59:43 -0300

    refactor: change date to date modified

--- src/components/tickets/table/index.tsx
+++ src/components/tickets/table/index.tsx
@@ -17,7 +17,7 @@ export default function TicketsTable({ tickets }: Props) {
       <thead>
         <tr className="border-b">
           <th className="text-left">Customer</th>
-          <th className="text-center">Date</th>
+          <th className="text-center">Date Modified</th>
           <th className="text-center">Status</th>
           <th className="text-right">#</th>
         </tr>
@@ -28,7 +28,7 @@ export default function TicketsTable({ tickets }: Props) {
           <tr key={ticket.id} className="border-b h-10">
             <td>{ticket.customer?.name}</td>
             <td className="text-center sm:table-cell hidden">
-              {ticket.created_at?.toLocaleDateString("en-US")}
+              {ticket.updated_at?.toLocaleDateString("en-US")}
             </td>
             <td>
               <div
