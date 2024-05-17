7547d81bdd98b08f0c70e0226a20b1372288de8c
bernardo damiani <bmdamiani17@gmail.com>
2024-05-17 09:39:46 -0300

    feature: open a ticket button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 import HomeImage from "@/assets/home-image.svg";
+import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="text-xl xs:mt-24 mt-20">Manage your company</h1>
+      <Link
+        href="/open"
+        className="bg-blue-500 text-white font-semibold rounded-lg p-2 mb-4 xs:mt-12 mt-8 hover:opacity-90 transition-all"
+      >
+        Open a ticket
+      </Link>
+      <h1 className="text-xl">Manage your company</h1>
       <h1 className="xs:text-3xl text-2xl font-semibold text-blue-700 ">
         Customers, Services
       </h1>
